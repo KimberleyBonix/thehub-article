23a4ac5931faa012961d3b11cf17e3924012d0f7
Kimberley Bonix <kimberley.bonix@oclock.school>
2024-01-14 22:32:00 +0100

    :sparkles: header integration

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const sidenav = document.querySelector(".sidenav");
-const openBtn = document.querySelector(".navigation__button--open");
-const closeBtn = document.querySelector(".navigation__button--close");
+const openBtn = document.querySelector(".navigation__open");
+const closeBtn = document.querySelector(".navigation__close");
 
 document.querySelector(".navigation").addEventListener("click", () => {
   sidenav.classList.toggle("hidden");
